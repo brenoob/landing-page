073868af322dbbe3044d369b1ede5f3bc6496d03
brenoob <breno_1993@outlook.com>
2024-05-04 09:17:29 -0300

    Update VideoArea.vue

--- ai-plataform/src/components/VideoArea.vue
+++ ai-plataform/src/components/VideoArea.vue
@@ -2,7 +2,7 @@
     <div class="w-screen flex items-start h-[735px]">
         <div class="flex px-0 items-end bg-primary mt-32 bg-bottom w-full h-[463px]">
             <video autoplay :controlslist="'nodownload'" :loop="true" controls
-                class="mx-auto w-[930px] h-[630px]" src="@/assets/video-example.mp4">
+                class="mx-auto w-[930px] h-[630px]" src="https://videos.pexels.com/video-files/3694426/3694426-hd_1920_1080_30fps.mp4">
             </video>
     
         </div>
