d0f1c70bde8d1d2f6b1011b68748e77e1ca5f69b
brenoob <breno_1993@outlook.com>
2024-05-10 10:48:13 -0300

    create AboutView

---
+++ ai-plataform/src/views/AboutView.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1> About </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus atque reiciendis recusandae eius, iusto accusantium debitis possimus! Quibusdam voluptas, a quaerat, iusto omnis, fugiat modi error culpa consequatur reiciendis repellendus.</p>
+</template>
