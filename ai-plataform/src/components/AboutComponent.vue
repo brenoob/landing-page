ea2063bcebab968c893f28c3c9a96444bfd01916
brenoob <breno_1993@outlook.com>
2024-05-10 12:08:07 -0300

    ending About

---
+++ ai-plataform/src/components/AboutComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="about h-screen flex flex-col justify-center items-center">
+        <h1 class="text-3xl mb-5 text-center text-primary"> About </h1>
+        <div class="flex bg-card rounded-lg p-4 mx-96 gap-10 items-center">
+            <img src="../assets/images/Assistant-1--Streamline-Milano.svg" alt="">
+    
+            <p class="text-text dark:text-textSecond">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus atque reiciendis recusandae eius, iusto accusantium debitis possimus! Quibusdam voluptas, a quaerat, iusto omnis, fugiat modi error culpa consequatur reiciendis repellendus.</p>
+        </div>
+
+    </div>
+</template>
--- ai-plataform/src/views/AboutView.vue
+++ ai-plataform/src/views/AboutView.vue
@@ -1,4 +1,8 @@
 <template>
-    <h1> About </h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus atque reiciendis recusandae eius, iusto accusantium debitis possimus! Quibusdam voluptas, a quaerat, iusto omnis, fugiat modi error culpa consequatur reiciendis repellendus.</p>
+    <AboutComponent />
 </template>
+
+<script setup lang="ts">
+import AboutComponent from '@/components/AboutComponent.vue';
+
+</script>
